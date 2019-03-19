4543e620cbf30485c2f85eaaccec96b442d4fea4
zhangbiao <18500199974@163.com>
2019-03-19 20:42:33 +0800

    部门类

---
+++ src/com/mybatis/entity/Department.java
@@ -0,0 +1,51 @@
+package com.mybatis.entity;
+
+import java.util.List;
+
+public class Department {
+
+	private Integer id;
+	private String deptName;
+	private List<Employee> emps;
+
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Department(Integer id, String deptName) {
+		super();
+		this.id = id;
+		this.deptName = deptName;
+	}
+
+	public List<Employee> getEmps() {
+		return emps;
+	}
+
+	public void setEmps(List<Employee> emps) {
+		this.emps = emps;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", deptName=" + deptName + ", emps=" + emps + "]";
+	}
+
+}
